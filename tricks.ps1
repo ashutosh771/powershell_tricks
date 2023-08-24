19852a64d6b39a819b77a074f980740fac259dc7
ashutosh771 <130644330+ashutosh771@users.noreply.github.com>
2023-08-25 04:45:02 +0530

    tricks.ps1

--- tricks.ps1
+++ tricks.ps1
@@ -1,3 +1,3 @@
-// Print current date, time and timezone so that it is visible in screenshots
+# Print current date, time and timezone so that it is visible in screenshots
 
 Write-Output ("Current system time is: $(Get-Date -Format "MMMM dd, yyyy HH:mm:ss"), Time Zone: $([System.TimeZoneInfo]::Local.Id)")
